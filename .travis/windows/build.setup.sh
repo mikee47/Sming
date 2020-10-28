4ce5bf4b14b8bbfeb8d7e1c0ad13dd27a2e13679
mikee47 <mike@sillyhouse.net>
2020-10-28 08:35:57 +0000

    z

--- .travis/windows/build.setup.sh
+++ .travis/windows/build.setup.sh
@@ -1,6 +1,3 @@
-export PATH=/C/MinGW/msys/1.0/bin:/C/MinGW/bin:/C/Python38:$PATH
-export PYTHON=/C/Python38/python
-
 # Keep paths short(ish)
 ln -s $TRAVIS_BUILD_DIR /c/z
 export SMING_HOME=/c/z/Sming
