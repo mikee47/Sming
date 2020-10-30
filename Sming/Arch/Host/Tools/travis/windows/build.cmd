4b2ddc3a006e9a818d45438070bc2c1b36496285
mikee47 <mike@sillyhouse.net>
2020-10-30 14:15:39 +0000

    Spawn a CMD shell, hopefully resolve conflict between git bash and MinGW

---
+++ Sming/Arch/Host/Tools/travis/windows/build.cmd
@@ -0,0 +1,22 @@
+REM Windows build script
+
+REM This will build the Basic_Blink application and most of the framework Components
+cd %SMING_PROJECTS_DIR%/samples/Basic_Blink
+make help
+make list-config
+%MAKE_PARALLEL% || goto :error
+
+cd %SMING_HOME%
+
+REM Build a couple of basic applications
+%MAKE_PARALLEL% Basic_Serial Basic_ProgMem STRICT=1 V=1 || goto :error
+
+REM Run basic tests
+%MAKE_PARALLEL% tests || goto :error
+
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
