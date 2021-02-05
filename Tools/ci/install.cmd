3a71868ae19109d5c9c6ae0a4e844286b8d6ea54
mikee47 <mike@sillyhouse.net>
2021-02-05 11:24:11 +0000

    x

--- Tools/ci/install.cmd
+++ Tools/ci/install.cmd
@@ -2,7 +2,7 @@ REM Windows install script
 
 set SMINGTOOLS=https://github.com/SmingHub/SmingTools/releases/download/1.0
 
-python -m pip install --upgrade pip -r ../requirements.txt
+python -m pip install --upgrade pip -r %SMING_HOME%\Tools\requirements.txt
 
 rmdir /s /q c:\MinGW
 curl -Lo MinGW.7z %SMINGTOOLS%/MinGW-2020-10-19.7z
