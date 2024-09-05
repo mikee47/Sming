5c236e583503546940fc7cc66008eb7892cdb648
mikee47 <mike@sillyhouse.net>
2024-09-05 14:12:27 +0100

    `Print` has its own float printer... replace with dtostrf()

--- Sming/Wiring/Print.cpp
+++ Sming/Wiring/Print.cpp
@@ -90,52 +90,7 @@ size_t Print::printNumber(const long long& num, uint8_t base, uint8_t width, cha
 
 size_t Print::printFloat(double number, uint8_t digits)
 {
-	size_t n = 0;
-
-	if(std::isnan(number)) {
-		return print("nan");
-	}
-	if(std::isinf(number)) {
-		return print("inf");
-	}
-	if(number > 4294967040.0) {
-		return print("ovf"); // constant determined empirically
-	}
-	if(number < -4294967040.0) {
-		return print("ovf"); // constant determined empirically
-	}
-
-	// Handle negative numbers
-	if(number < 0.0) {
-		n += print('-');
-		number = -number;
-	}
-
-	// Round correctly so that print(1.999, 2) prints as "2.00"
-	double rounding = 0.5;
-	for(uint8_t i = 0; i < digits; ++i) {
-		rounding /= 10.0;
-	}
-
-	number += rounding;
-
-	// Extract the integer part of the number and print it
-	unsigned long int_part = number;
-	auto remainder = number - int_part;
-	n += print(int_part);
-
-	// Print the decimal point, but only if there are digits beyond
-	if(digits > 0) {
-		n += print('.');
-	}
-
-	// Extract digits from the remainder one at a time
-	while(digits-- > 0) {
-		remainder *= 10.0;
-		int toPrint = remainder;
-		n += print(toPrint);
-		remainder -= toPrint;
-	}
-
-	return n;
+	char buf[64];
+	dtostrf(number, 0, digits, buf);
+	return print(buf);
 }
